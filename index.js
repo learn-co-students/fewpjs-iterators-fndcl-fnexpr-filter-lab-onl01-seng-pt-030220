f2700b03afc4b5ea148f62dffb568cf701466bd4
MMcClure11 <mcclme02@luther.edu>
2020-09-03 09:53:35 -0500

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function fuzzyMatch(drivers, string) {
 function matchName(drivers, string) {
   return drivers.filter(driver => driver.name === string)
 }
+
+/////Learn co solution
+
+// function findMatching(source, sought) {
+//   return source.filter( possibleMatch =>
+//     possibleMatch.toLowerCase() === sought.toLowerCase()
+//   )
+// }
+
+// function fuzzyMatch(arrayOfDrivers, letter) {
+//   return arrayOfDrivers.filter(driver => driver.slice(0, letter.length).toUpperCase() === letter.toUpperCase())
+// }
+
+// function matchName(source, soughtName) {
+//   return source.filter( record => record.name === soughtName)
+// }
