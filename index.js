91a67b7acacb49dbe4148fa4cbbf452b4054e0be
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-09-27 17:57:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+let drivers = ['marcos', 'vanessa', 'murphy', 'casey', 'vans', 'vizcaino']
+
+function findMatching(drivers, string){
+  return drivers.filter(driver=> driver.toLowerCase() === string.toLowerCase() )
+}
+
+// console.log(findMatching(drivers,'VANESSA'))
+
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(driver=> driver[0] === string[0] )
+  // return drivers.filter(driver=> driver.charAt(0) === string.charAt(0) ) 
+}
+
+// console.log(fuzzyMatch(drivers, 'marcos'))
+
+
+function matchName(drivers, string){
+  return drivers.filter(driver=> driver.name === string )
+}
