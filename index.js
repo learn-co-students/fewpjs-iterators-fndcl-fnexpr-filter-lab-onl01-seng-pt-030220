d0c26e1888b52ade690c0a0dcc5f525d2eaa2624
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-09-27 17:58:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 
-let drivers = ['marcos', 'vanessa', 'murphy', 'casey', 'vans', 'vizcaino']
 
 function findMatching(drivers, string){
   return drivers.filter(driver=> driver.toLowerCase() === string.toLowerCase() )
 }
 
-// console.log(findMatching(drivers,'VANESSA'))
 
 
 function fuzzyMatch(drivers, string){
@@ -13,7 +11,6 @@ function fuzzyMatch(drivers, string){
   // return drivers.filter(driver=> driver.charAt(0) === string.charAt(0) ) 
 }
 
-// console.log(fuzzyMatch(drivers, 'marcos'))
 
 
 function matchName(drivers, string){
